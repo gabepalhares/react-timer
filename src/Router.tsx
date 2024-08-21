8ef739a9a5fe54979525b3dab36977c560daf55e
minortypo <gabrielpalharesdev@gmail.com>
2024-08-21 15:37:16 -0300

    refactor(typo): fixes small typo in element property

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { DefaultLayot } from './layouts/DefaultLayout';
+import { DefaultLayout } from './layouts/DefaultLayout';
 
 import { Home } from './pages/Home';
 import { History } from './pages/History';
@@ -7,7 +7,7 @@ import { History } from './pages/History';
 export function Router() {
     return (
         <Routes>
-            <Route path="/" element={<DefaultLayot />}>
+            <Route path="/" element={<DefaultLayout />}>
                 <Route path="/" element={<Home />} />
                 <Route path="/history" element={<History />} />
             </Route>
