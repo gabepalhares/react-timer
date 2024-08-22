12c64b6ba07c8de714785819ef06bf3ef05745ae
minortypo <gabrielpalharesdev@gmail.com>
2024-08-22 10:57:07 -0300

    feat: adds datalist options and configs minutes amount input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,14 +9,25 @@ export function Home() {
           <label htmlFor="task">I'm going to work on</label>
           <TaskInput
             id="task"
+            list="task-suggestions"
             placeholder="Give a name to your task"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Jujutsu Kaisen" />
+          </datalist>
 
           <label htmlFor="minutesAmount">for</label>
           <MinutesAmountInput
             id="minutesAmount"
             type="number"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutes.</span>
